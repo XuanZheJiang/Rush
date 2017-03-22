a9b20105c80d81b456e07e398b19dd7a24133bf3
蒋轩哲 <jgcm@live.cn>
2017-03-22 13:30:47 +0800

    add Score

--- Rush/Rush/GameScene.swift
+++ Rush/Rush/GameScene.swift
@@ -15,7 +15,17 @@ class GameScene: SKScene {
     var mine = SKSpriteNode()
     var enemy = SKSpriteNode()
     
+    var mineL = SKLabelNode()
+    var enemyL = SKLabelNode()
+    
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        
+        startGame()
+        
+        enemyL = self.childNode(withName: "enemyL") as! SKLabelNode
+        mineL = self.childNode(withName: "mineL") as! SKLabelNode
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         mine = self.childNode(withName: "mine") as! SKSpriteNode
@@ -29,6 +39,28 @@ class GameScene: SKScene {
         
         self.physicsBody = border
         
+    }
+    
+    func startGame() {
+        score = [0,0]
+        enemyL.text = "\(score[1])"
+        mineL.text = "\(score[0])"
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode) {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == mine {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        } else if playerWhoWon == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        
+        enemyL.text = "\(score[1])"
+        mineL.text = "\(score[0])"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -51,7 +83,12 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= mine.position.y - 70 {
+            addScore(playerWhoWon: enemy)
+        } else if ball.position.y >= enemy.position.y + 70{
+            addScore(playerWhoWon: mine)
+        }
     }
 }
